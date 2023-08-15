6d48e94aba8f948d82875ca40eabe7be4082f02d
baharma <bagusaditya1899@gmail.com>
2023-08-15 09:53:53 +0800

    tamplate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,12 +12,11 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
-
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
