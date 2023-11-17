b2f9831492e9412a4428448d51406c3a1e40ed7f
Baharma <bagusaditya1899@gmail.com>
2023-11-17 14:59:26 +0800

    judul update

--- resources/views/pages/component-boostrap/modal-penjualan.blade.php
+++ resources/views/pages/component-boostrap/modal-penjualan.blade.php
@@ -48,6 +48,7 @@
                     </div>
                     <div class="row">
                         <div class="col">
+                            <label for="">Piutang</label>
                             <x-layout.input-groub-button getId="penjualan" canvas="#offcanvasPiutang" />
                             <small>isi ini bagian trakhir</small>
                         </div>
