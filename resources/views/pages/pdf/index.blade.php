56ed76229b92affba02832d8eba743e94f8a7e27
Baharma <bagusaditya1899@gmail.com>
2023-11-17 16:53:26 +0800

    commit

--- resources/views/pages/pdf/index.blade.php
+++ resources/views/pages/pdf/index.blade.php
@@ -10,7 +10,7 @@
             <div class="p-4 bd-highlight" style="font-size: 20px; text-align: center">
                 <i class="bi bi-file-earmark-pdf-fill" style="font-size: 150px;"></i>
                 <a href="#" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalPenjualan">
-                    <h3>Laporan Penjualan</h3>
+                    <h4>Laporan Penjualan</h4>
                 </a>
             </div>
         </div>
@@ -18,7 +18,7 @@
             <div class="p-4 bd-highlight" style=" text-align: center">
                 <i class="bi bi-file-earmark-pdf-fill" style="font-size: 150px;"></i>
                 <a href="#" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalpengeluaran">
-                    <h3>Laporan Pengeluaran</h3>
+                    <h4>Laporan Pengeluaran</h4>
                 </a>
             </div>
         </div>
@@ -26,7 +26,7 @@
             <div class="p-4 bd-highlight" style="font-size: 20px; text-align: center">
                 <i class="bi bi-file-earmark-pdf-fill" style="font-size: 150px;"></i>
                 <a href="" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalnaraca">
-                    <h3>Laporan Neraca</h3>
+                    <h4>Laporan Neraca</h4>
                 </a>
             </div>
         </div>
@@ -34,7 +34,7 @@
             <div class="p-4 bd-highlight" style="font-size: 20px; text-align: center">
                 <i class="bi bi-file-earmark-pdf-fill" style="font-size: 150px;"></i>
                 <a href="" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modallabarugi">
-                    <h3>Laporan Laba Rugi</h3>
+                    <h4>Laporan Laba Rugi</h4>
                 </a>
             </div>
         </div>
