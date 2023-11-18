28784f16af5563e2c8f782b8c7cbeda5f6ff3acb
baharma <bagusaditya1899@gmail.com>
2023-11-18 12:43:37 +0800

    add

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/', function () {
         if (Auth::check()) {
-            if (Auth::user()->role == 'kasir') {
+            if (Auth::user()->role == 'kasir' || Auth::user()->role == 'admin') {
                 return redirect()->route('dashboard.kasir');
             }
         }
