5efbf802b09c23a069fdefd94ddc35bb86fff066
Kadek Satya <kadekrestusatyawardana@gmail.com>
2023-11-22 03:10:04 +0800

    update

--- database/migrations/2023_11_17_204118_add__fullname_to__user.php
+++ database/migrations/2023_11_17_204118_add__fullname_to__user.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('fullname')->default('');
+            $table->text('fullname')->default(null);
         });
     }
 
