ffa8597d71d26ea8ad947eb0024e1e98c3122814
Kadek Satya <kadekrestusatyawardana@gmail.com>
2023-11-23 15:51:27 +0800

    fix

--- resources/views/pages/balancesheet/print.blade.php
+++ resources/views/pages/balancesheet/print.blade.php
@@ -203,12 +203,12 @@
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 
-    {{-- <script>
+    <script>
         window.onload = function () {
             window.print();
         };
 
-    </script> --}}
+    </script>
 </body>
 
 </html>
