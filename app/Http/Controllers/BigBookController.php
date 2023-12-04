bd18a3f28861ad00bd9f5e5b9116efa58b5569b2
Kadek Satya <kadekrestusatyawardana@gmail.com>
2023-12-04 19:58:35 +0800

    update date time

--- app/Http/Controllers/BigBookController.php
+++ app/Http/Controllers/BigBookController.php
@@ -20,6 +20,9 @@ class BigBookController extends Controller
         $start = $request->start_date;
         $end = $request->end_date;
 
+        // Make sure to include the end of the day for the end date
+        $endOfDay = date('Y-m-d 23:59:59', strtotime($end));
+
         $coa = Akun::all();
         $data = JournalItem::orderBy('created_at', 'ASC')->with(['journal', 'coa'])->whereBetween('created_at', [$start, $end])->get();
 
